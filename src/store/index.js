f9f136b68cf69b70929736434d514e1a78436881
Tvopi <payko-v@bk.ru>
2023-03-14 18:36:04 +0800

    добавлены мужские категории и тавары

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,5 @@
 // import { store } from 'quasar/wrappers'
-import { createStore } from 'vuex'
+import { createStore } from "vuex";
 // import { st } from './st'
 import { v4 as uuidv4 } from "uuid";
 
@@ -14,7 +14,7 @@ export default createStore({
         todayprice: 61990,
         type: "Электрическая",
         speed: 22,
-        option: "Есть"
+        option: "Есть",
       },
       {
         id: uuidv4(),
@@ -24,7 +24,7 @@ export default createStore({
         todayprice: 61990,
         type: "Электрическая",
         speed: 22,
-        option: "Есть"
+        option: "Есть",
       },
       {
         id: uuidv4(),
@@ -34,7 +34,7 @@ export default createStore({
         todayprice: 61990,
         type: "Электрическая",
         speed: 22,
-        option: "Есть"
+        option: "Есть",
       },
       {
         id: uuidv4(),
@@ -44,21 +44,151 @@ export default createStore({
         todayprice: 61990,
         type: "Электрическая",
         speed: 22,
-        option: "Есть"
-      }
-    ]
+        option: "Есть",
+      },
+    ],
+
+    men: {
+      breeches: [
+        {
+          id: uuidv4(),
+          pretitle: "Крутые бриджи",
+          title: "Бриджи",
+          oldprice: 2000,
+          todayprice: 1599,
+          option: "Есть",
+        },
+        {
+          id: uuidv4(),
+          pretitle: "Крутые бриджи",
+          title: "Бриджи",
+          oldprice: 2300,
+          todayprice: 1799,
+          option: "Есть",
+        },
+        {
+          id: uuidv4(),
+          pretitle: "Крутые бриджи",
+          title: "Бриджи",
+          oldprice: 2600,
+          todayprice: 1999,
+          option: "Есть",
+        },
+      ],
+      headdress: [
+        {
+          id: uuidv4(),
+          pretitle: "Крутая кепка",
+          title: "Кепка",
+          oldprice: 2000,
+          todayprice: 1599,
+          option: "Есть",
+        },
+        {
+          id: uuidv4(),
+          pretitle: "Крутая шляпа",
+          title: "Шляпа",
+          oldprice: 2000,
+          todayprice: 1599,
+          option: "Есть",
+        },
+        {
+          id: uuidv4(),
+          pretitle: "Крутая панама",
+          title: "Панама",
+          oldprice: 2000,
+          todayprice: 1599,
+          option: "Есть",
+        },
+      ],
+      shirts: [
+        {
+          id: uuidv4(),
+          pretitle: "Крутая рубашка",
+          title: "Рубашка",
+          oldprice: 2000,
+          todayprice: 1599,
+          option: "Есть",
+        },
+        {
+          id: uuidv4(),
+          pretitle: "Крутая рубашка",
+          title: "Рубашка",
+          oldprice: 3000,
+          todayprice: 2599,
+          option: "Есть",
+        },
+        {
+          id: uuidv4(),
+          pretitle: "Крутая рубашка",
+          title: "Рубашка",
+          oldprice: 2000,
+          todayprice: 1899,
+          option: "Есть",
+        },
+      ],
+      t_shirts: [
+        {
+          id: uuidv4(),
+          pretitle: "Крутая футболка",
+          title: "Футболка",
+          oldprice: 2000,
+          todayprice: 1899,
+          option: "Есть",
+        },
+        {
+          id: uuidv4(),
+          pretitle: "Крутая футболка",
+          title: "Футболка",
+          oldprice: 3000,
+          todayprice: 2899,
+          option: "Есть",
+        },
+        {
+          id: uuidv4(),
+          pretitle: "Крутая футболка",
+          title: "Футболка",
+          oldprice: 1000,
+          todayprice: 789,
+          option: "Есть",
+        },
+      ],
+      tights: [
+        {
+          id: uuidv4(),
+          pretitle: "Крутые тайтсы",
+          title: "Тайтсы",
+          oldprice: 2600,
+          todayprice: 1999,
+          option: "Есть",
+        },
+        {
+          id: uuidv4(),
+          pretitle: "Крутые тайтсы",
+          title: "Тайтсы",
+          oldprice: 1600,
+          todayprice: 1399,
+          option: "Есть",
+        },
+        {
+          id: uuidv4(),
+          pretitle: "Крутые тайтсы",
+          title: "Тайтсы",
+          oldprice: 1600,
+          todayprice: 1199,
+          option: "Есть",
+        },
+      ],
+    },
+
+    women: {},
   },
   getters: {
     PRODUCTS(state) {
-      return state.products
+      return state.products;
     },
   },
-  mutations: {
-
-  },
-  actions: {
-
-  },
-  modules: {
-  }
-})
+  mutations: {},
+  actions: {},
+  modules: {},
+});
