23ba29c42b04c4d71614b2fdfdee5206187bbe7b
Gelato <you@example.com>
2023-03-15 02:34:05 +0300

    данные получаются из бд

--- src/components/v-catalog-slide.vue
+++ src/components/v-catalog-slide.vue
@@ -2,8 +2,7 @@
   <div class="proudcts-slider-page__wrapper">
     <div class="proudcts-slider-page__product product">
       <a href="#" class="product__img">
-        <img src="../assets/Catalog/imgItem.webp" alt="not found" />
-        
+        <img :src="require('../assets/Catalog/' + product.url)" />
       </a>
       <div class="product__title">
         <p>
@@ -25,7 +24,6 @@
       </div>
     </div>
     <div class="proudcts-slider-page__product-hover hover-product">
-      
       <div class="hover-product__title">
         {{ product.pretitle }}
         <p>{{ product.title }}</p>
@@ -47,7 +45,6 @@
       <a href="#" class="hover-product__cart">
         <div>
           <img src="../assets/Catalog/cart_orange.webp" alt="" />
-
         </div>
       </a>
       <div class="hover-product__footer">
@@ -76,16 +73,36 @@ export default {
     },
   },
   setup(props) {},
-
-   
 };
 </script>
 
 <style lang="scss">
 .proudcts-slider-page {
+  &__slider-catalog {
+    .product__todayprice {
+      font-family: Roboto-Black;
+    }
+    .product__title {
+      text-align: left;
+      margin: 0 0 0px 0;
+      a {
+        font-size: 15px;
+        text-transform: none;
+        font-family: Roboto-Black;
+        span {
+          text-transform: inherit;
+          font-size: 15px;
+          display: block;
+          color: inherit;
+          font-family: Roboto-Light;
+        }
+      }
+    }
+  }
+
   &__wrapper {
     text-align: center;
-    max-width: 265px;
+    width: 265px;
     height: 385px;
     position: relative;
   }
@@ -99,13 +116,63 @@ export default {
     width: 100%;
     height: 100%;
   }
-  &__item {
-    overflow: hidden;
-    width: 49%;
-    display: inline-block;
-    @media (max-width: 780px) {
+
+  &__slider {
+    .product__todayprice {
+      font-family: Roboto-Black;
+    }
+    .product__title {
+      text-align: left;
+      margin: 0 0 0px 0;
+      a {
+        font-size: 15px;
+        text-transform: none;
+        font-family: Roboto-Black;
+        span {
+          text-transform: inherit;
+          font-size: 15px;
+          display: block;
+          color: inherit;
+          font-family: Roboto-Light;
+        }
+      }
+    }
+    .hover-product {
+      text-align: left;
+    }
+    max-width: 960px;
+    @media (max-width: 1295px) {
+      max-width: 900px;
+    }
+    @media (max-width: 1235px) {
+      max-width: 820px;
+    }
+    @media (max-width: 1150px) {
+      max-width: 740px;
+    }
+    @media (max-width: 1070px) {
+      max-width: 670px;
+    }
+    @media (max-width: 992px) {
+      max-width: 960px;
+    }
+    .slick-track {
+      display: flex;
       width: 100%;
     }
+    .slick-list {
+      overflow: hidden;
+    }
+    .slick-slide {
+      overflow: hidden;
+      div {
+      }
+    }
+  }
+
+  &__item {
+    overflow: hidden;
+    flex: 1 1 auto;
     .hover-product {
       transition-duration: 0.3s;
       left: -100%;
@@ -131,6 +198,35 @@ export default {
     .product__img {
       width: 100%;
       height: 213px;
+    }
+  }
+
+  &__select {
+    display: flex;
+    justify-content: space-between;
+    padding: 12px 0 12px 16px;
+    border-bottom: solid 1px #ededed;
+    border-top: solid 1px #ededed;
+    @media (max-width: 540px) {
+      display: block;
+    }
+  }
+
+  &__label {
+    font-family: Roboto-Black;
+    font-size: 14px;
+    line-height: 18px;
+    color: #333333;
+    margin: 0 15px 0 0;
+  }
+
+  &__wrapper-select {
+    select {
+      padding: 4px 2px 4px 8px;
+      font-family: Roboto-Black;
+      font-size: 16px;
+      line-height: 1.2;
+      color: #999999;
     }
   }
 }
--- src/pages/v-catalog.vue
+++ src/pages/v-catalog.vue
@@ -1,54 +1,215 @@
 <template>
-  <div class=""></div>
+  <div class="catalog">
+    <div class="catalog__panel-options">
+      <div class="q-pa-md" style="max-width: 300px">
+        <div class="q-gutter-md">
+          <q-badge color="secondary" multi-line> "{{ model }}" </q-badge>
+
+          <q-select
+            filled
+            v-model="model"
+            :options="options"
+            label="Multi with toggle"
+            multiple
+            emit-value
+            map-options
+          >
+            <template
+              v-slot:option="{ itemProps, opt, selected, toggleOption }"
+            >
+              <q-item v-bind="itemProps">
+                <q-item-section>
+                  <q-item-label v-html="opt.label" />
+                </q-item-section>
+                <q-item-section side>
+                  <q-toggle
+                    :model-value="selected"
+                    @update:model-value="toggleOption(opt)"
+                  />
+                </q-item-section>
+              </q-item>
+            </template>
+          </q-select>
+        </div>
+      </div>
+    </div>
+    <div class="catalog__wrap">
+      <Slide
+        v-for="product in products.values"
+        :key="product.id"
+        :product="product"
+      />
+    </div>
+  </div>
 </template>
 
 <script>
-import { ref } from "vue";
+import { watch, ref, computed, reactive, onMounted } from "vue";
 import { useStore } from "vuex";
 import Slide from "../components/v-catalog-slide.vue";
-import { useQuery } from "@vue/apollo-composable";
+import { useQuery, provideApolloClient } from "@vue/apollo-composable";
 import gql from "graphql-tag";
+import { useMutation } from "@vue/apollo-composable";
 
 export default {
   components: {
     Slide,
   },
   setup() {
-    const $store = useStore();
-    const products = $store.state.products;
+    const products = reactive([]);
+    const model = ref([]);
+
+    watch(model, () => {
+      const { result, loading, error } = useQuery(gql`
+        query MyQuery {
+          products(
+            where: {
+              category: { _eq: "Бриджи" }
+              _and: { gender: { _eq: true } }
+            }
+          ) {
+            category
+            discount
+            gender
+            id
+            name
+            oldprice
+            todayprice
+            url
+          }
+        }
+      `);
+      products.values = result?.value?.products;
+      console.log(result?.value?.products);
+    });
 
     const { result, loading, error } = useQuery(gql`
       query MyQuery {
         products {
           category
+          discount
           gender
           id
           name
           oldprice
           todayprice
+          url
         }
       }
     `);
-    console.log(result.value);
+    products.values = result?.value?.products;
 
     return {
       slide: ref(1),
       products,
-      result,
-      loading,
-      error,
+      model,
+
+      options: [
+        {
+          label: "Мужская одежда",
+          value: 1,
+        },
+        {
+          label: "Женская",
+          value: 2,
+        },
+        {
+          label: "По цене",
+          value: 3,
+        },
+        {
+          label: "Apple",
+          value: "123",
+        },
+        {
+          label: "Oracle",
+          value: 5,
+        },
+      ],
     };
   },
 };
 </script>
 
 <style scoped lang="scss">
-.wrap {
+.order-catalog {
   display: flex;
-  justify-content: space-between;
-  @media (max-width: 780px) {
-    flex-direction: column;
-    align-items: center;
+  align-items: center;
+  @media (max-width: 530px) {
+    margin: 0 0 16px 0;
+    display: block;
+  }
+  &__label {
+    font-family: Roboto-Black;
+    font-size: 14px;
+    line-height: 18px;
+    color: #333333;
+    margin: 0 34px 0 0;
+    @media (max-width: 530px) {
+      display: block;
+      margin: 0 0 7px 0;
+    }
+  }
+
+  &__select {
+    color: #0097ac;
+    select {
+      color: #0097ac;
+      width: 120px;
+      @media (max-width: 530px) {
+        width: 100%;
+      }
+    }
+  }
+
+  &__item {
+    font-family: Roboto-Light;
+    font-size: 14px;
+    line-height: 1.2;
+    color: #0097ac;
+  }
+}
+.catalog {
+  margin: 180px 0 0 0;
+  background-color: #e4dcdc;
+  &__wrap {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    @media (max-width: 780px) {
+      flex-direction: column;
+      align-items: center;
+    }
+  }
+  &__panel-options {
+    width: 100%;
+    height: 100px;
+  }
+  margin: 0 0 15px 0;
+  &__title {
+    font-family: Roboto-Black;
+    font-size: 30px;
+    line-height: 1.2;
+    text-transform: uppercase;
+    letter-spacing: 0.025em;
+    color: #dadada;
+    margin: 0 0 24px 0;
+  }
+
+  &__action {
+    display: flex;
+    justify-content: space-between;
+    background-color: #f3f3f3;
+    padding: 7px 17px;
+    @media (max-width: 530px) {
+      display: block;
+    }
+  }
+
+  &__order {
+  }
+
+  &__view {
   }
 }
 </style>
