a8ef435cb7ba01f0c9a998d510c0aa1e5fe47cd1
Tvopi <payko-v@bk.ru>
2023-03-15 18:16:41 +0800

    изменение названий в мейн лаунчере

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -14,19 +14,19 @@
         <q-item-section avatar>
           <q-icon name="style" color="white" />
         </q-item-section>
-        <q-item-section> Main </q-item-section>
+        <q-item-section> Главное </q-item-section>
       </q-item>
       <q-item to="/catalog" exact clickable v-ripple>
         <q-item-section avatar>
           <q-icon name="style" color="white" />
         </q-item-section>
-        <q-item-section> Catalog </q-item-section>
+        <q-item-section> Каталог </q-item-section>
       </q-item>
       <q-item to="/cart" exact clickable v-ripple>
         <q-item-section avatar>
           <q-icon name="style" color="white" />
         </q-item-section>
-        <q-item-section> Cart </q-item-section>
+        <q-item-section> Корзина </q-item-section>
       </q-item>
     </q-drawer>
 
